ac3fb81c025526e336d7c5047700fa1197f11754
root <root@ip-172-31-18-51.ap-south-1.compute.internal>
2019-04-15 09:34:49 +0000

    compose

---
+++ compose.sh
@@ -0,0 +1,6 @@
+sed "s/myservices/$myservices/" compose.sh
+sed "s/img/$img/" compose.sh
+sed "s/r1/$r1/" compose.sh
+sed "s/cp/$cp/" compose.sh
+sed "s/ms/$ms/" compose.sh
+sed "s/hp/$hp/" compose.sh
