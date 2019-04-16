7f1ea23da501bd83c233c3e1e86383ef9e3019e4
rama7674 <48182178+rama7674@users.noreply.github.com>
2019-04-16 10:01:14 +0530

    Update compose.sh

--- compose.sh
+++ compose.sh
@@ -1,6 +1,2 @@
-sed "s/myservices/$myservices/" compose.sh
-sed "s/img/$img/" compose.sh
-sed "s/r1/$r1/" compose.sh
-sed "s/cp/$cp/" compose.sh
-sed "s/ms/$ms/" compose.sh
-sed "s/hp/$hp/" compose.sh
+sed "s/myservices/$myservices/g;s/img/$img/g;s/r1/$r1/g;s/ms/$ms/g" compose.sh
+
