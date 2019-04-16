1cd565e0f11fb295fbd7f793fb71b25065b03207
rama7674 <48182178+rama7674@users.noreply.github.com>
2019-04-16 21:45:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 touch img1
 docker search jboss > img1
-VAR="$(tail -25 img | head -1 | cut  -c1-30)"
-echo $VAR
-sed 's/y/'$VAR'/' pipeline
+tail -25 img | head -1 | cut  -c1-30 > img2
+var= $( cat img2 )
+echo $var
+sed 's/y/$var/' pipeline
